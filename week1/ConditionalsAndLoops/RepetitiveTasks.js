495c54cd973ceffb17d4f677fe2cd0058855559d
Zumh <17211423+Zumh@users.noreply.github.com>
2023-10-27 13:25:33 -0400

    done with js

---
+++ week1/ConditionalsAndLoops/RepetitiveTasks.js
@@ -0,0 +1,92 @@
+/*Task 1
+Write a "for" loop that will perform exactly the same repetitive code as this:
+*/
+/*
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+*/
+for(var number = 1; number <= 5; number++){
+    console.log(number);
+}
+console.log('Counting completed!')
+
+
+/*
+Task 2 
+Write a "for" loop that will perform exactly the same repetitive code as this:
+*/
+/*
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)*/
+for(var number = 5; number >= 1; number--){
+    console.log(number);
+}
+
+console.log('Countdown finished!')
+
+/*
+Task 3
+Write a "while" loop that will perform exactly the same repetitive code as this:
+*/
+
+/*
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+*/
+var number = 1;
+while(number <= 5){
+    console.log(number);
+    number++;
+}
+console.log('Counting completed!')
+
+
+//Note: Name your increment variable i. Update the variable in the while loop using i++.
+
+/*
+Task 4
+Write a "while" loop that will perform exactly the same repetitive code as this:
+*/
+/*
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+*/
+var number = 5;
+while(number >= 1){
+    console.log(number);
+    number--;
+}
+console.log('Countdown finished!')
+
+
+//Note: In the while loop, decrement the value of i using: i = i - 1.
+
+/*
+Task 5
+Write a "while" loop that will perform exactly the same repetitive code as this:
+*/
+/*
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+*/
+var year = 2018;
+while (year <= 2022){
+
+    console.log(year);
+    year++;
+}
