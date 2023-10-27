e9ebbe8fe63f6f4a7f8ddd737beebbef2523e0f4
Zumh <17211423+Zumh@users.noreply.github.com>
2023-10-27 14:10:32 -0400

    done with nested loops js

--- week1/Loops.js
+++ week1/Loops.js
@@ -46,3 +46,20 @@ When you code the solution, the output in the console should remain exactly the 
 
 Note: You'll need three separate cases for the three medals, and a default case for all other values of the i variable.
  */
+
+for(let i = 1; i <= 10; i++){
+    switch(i){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(i);
+            break;
+    }
+}
